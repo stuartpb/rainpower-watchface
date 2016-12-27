a872b492e8e661a01ce4569ae6d6b46b9d812787
Stuart P. Bentley <stuart@testtrack4.com>
2016-12-27 15:30:37 +0000

    Use Nova Mono

--- src/c/main.c
+++ src/c/main.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm structure
@@ -30,10 +31,15 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NOVA_MONO_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
@@ -68,6 +74,8 @@ static void init() {
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
 }
 
 int main(void) {
