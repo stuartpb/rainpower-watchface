a01dce49b99f7b008c7c1b71b5551b069981fb54
Stuart P. Bentley <stuart@testtrack4.com>
2017-01-05 08:59:57 +0000

    Add initial configuration

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,10 +1,22 @@
-/* global Pebble navigator */
+/* global Pebble navigator localStorage */
+
+// Import the Clay package
+var Clay = require('pebble-clay');
+// Load our Clay configuration file
+var clayConfig = require('./config.js');
+// Initialize Clay
+var clay = new Clay(clayConfig, null, {autoHandleEvents: false});
+
 function pebbleSuccess(e) {
   // do nothing
 }
 function pebbleFailure(e) {
   console.error(e);
 }
+
+// See webviewclosed handler below
+var darkSkyApiKey = localStorage.getItem('DARK_SKY_API_KEY');
+var darkSkyRequestUnits = localStorage.getItem('DARK_SKY_REQUEST_UNITS');
 
 var reportPhoneBatt;
 
@@ -32,4 +44,32 @@ Pebble.addEventListener('appmessage', function(e) {
   if (e.payload.QUERY_PHONE_BATT && reportPhoneBatt) {
     return reportPhoneBatt();
   }
+});
+
+// Handle Clay events manually
+Pebble.addEventListener('showConfiguration', function(e) {
+  // our showConfiguration looks basically normal
+  Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+
+  if (e && !e.response) {
+    return;
+  }
+
+  // Get the keys and values from each config item
+  var dict = clay.getSettings(e.response, false);
+
+  // Since most (all, at time of writing) or our config options affect the
+  // behavior of the JS component and not the watch, we don't send an
+  // appMessage to the watch with them the way the default handler does.
+  // Instead, we update our local variables reflecting them, and serialize
+  // them out to their own localStorage options.
+  // This is admittedly redundant to what Clay does with 'em, but /shrug
+
+  darkSkyApiKey = dict.DARK_SKY_API_KEY;
+  localStorage.setItem('DARK_SKY_API_KEY', darkSkyApiKey);
+  darkSkyRequestUnits = dict.DARK_SKY_REQUEST_UNITS;
+  localStorage.setItem('DARK_SKY_REQUEST_UNITS', darkSkyRequestUnits);
 });
